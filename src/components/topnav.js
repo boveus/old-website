29c7d44afd38f30ed64380ddc8f32f14fbdeef69
Brandon Stewart <boveus@gmail.com>
2018-03-10 12:36:55 -0500

    add topnav component without errors

---
+++ src/components/topnav.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class Topnav extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+    }
+  }
+  render () {
+    return (
+      <nav>
+        <h3> This is a test of the top nav </h3>
+      </nav>
+    )
+  }
+}
+
+export default Topnav
