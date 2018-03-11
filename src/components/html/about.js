02c0ea3a8fed6e267ba315070bdcaec34bb3603a
Brandon Stewart <boveus@gmail.com>
2018-03-11 15:19:18 -0400

    add project description modal and project caurosel

--- src/components/html/about.js
+++ src/components/html/about.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
+import { Image } from 'react-bootstrap'
 
 class About extends Component {
   render () {
     return (
       <div className='about-page'>
-        <h2> You are on the about Page! </h2>
+        <article>
+          <h1>About me</h1>
+          <p>
+            My name is Brandon Stewart.  I am an aspiring software developer, cyclist, and beer snob.  This is a website for posting blog posts about various things, technological or otherwise.
+          </p>
+          <h4>Vail Pass</h4>
+          <Image src='image/bike_pic.jpg' responsive rounded className='bike-pic' />;
+        </article>
       </div>
     )
   }
--- src/components/html/blog.js
+++ src/components/html/blog.js
@@ -3,7 +3,14 @@ import React, { Component } from 'react'
 class Blog extends Component {
   render () {
     return (
-      <h2> You are on the Blog page! </h2>
+      <article>
+        <h2>General Computer Science Topics</h2>
+        <a href='recursion-blog-post.html'>Recursion</a>
+        <h2>Network Security</h2>
+        <a href='security-resources.html'>Getting Started in Network Security - Gist</a>
+        <h2>Thoughts on my Open Source Contribution </h2>
+        <a href='open-source.html'>My first open source PR</a>
+      </article>
     )
   }
 }
