50f691fa30d18b60db06313c423219be73b209cf
Brandon Stewart <boveus@gmail.com>
2018-04-11 22:44:03 -0400

    add padding

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Footer from './components/footer'
 class App extends Component {
   render () {
     return (
-      <div>
+      <div id='main'>
         <div className='row'>
           <Header />
         </div>
